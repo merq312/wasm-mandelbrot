5749cc432f6acd2e44df1520ab383d77794b878d
Chamila Gunasena <merc312@gmail.com>
2021-10-05 20:34:18 -0400

    first working build

--- src/lib.rs
+++ src/lib.rs
@@ -9,55 +9,41 @@ use wasm_bindgen::prelude::*;
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
-#[wasm_bindgen]
-extern "C" {
-    fn alert(s: &str);
-}
-
-#[wasm_bindgen]
-pub fn greet() {
-    alert("Hello, wasm-mandelbrot!");
-}
-
-const WIDTH: usize = 800;
-const HEIGHT: usize = 600;
 const MAX_ITERATIONS: usize = 1000;
 
 #[wasm_bindgen]
-struct Mandelbrot {
+pub struct Mandelbrot {
     fractal: Vec<u32>,
     hist: Vec<u32>,
+    mem: Vec<u8>,
     total: u32,
-}
-
-#[wasm_bindgen]
-struct CoordinatePair {
-    x1: f32,
-    x2: f32,
-    y1: f32,
-    y2: f32,
-}
-
-#[wasm_bindgen]
-impl CoordinatePair {
-    pub fn transform_coords(&mut self) {
-        // FIXME: Why are all coords divided by height???
-        self.x1 = 2.0 * self.x1 / HEIGHT as f32 - 2.0;
-        self.x2 = 2.0 * self.x2 / HEIGHT as f32 - 2.0;
-        self.y1 = 2.0 * self.y1 / HEIGHT as f32 - 1.0;
-        self.y2 = 2.0 * self.y2 / HEIGHT as f32 - 1.0;
-    }
+    width: usize,
+    height: usize,
 }
 
 #[wasm_bindgen]
 impl Mandelbrot {
-    pub fn new(&mut self) {
-        self.fractal = vec![0; WIDTH * HEIGHT];
-        self.hist = vec![0; MAX_ITERATIONS];
-        self.total = 0;
+    pub fn new(width: usize, height: usize) -> Mandelbrot {
+        let fractal = vec![0; width * height];
+        let hist = vec![0; MAX_ITERATIONS];
+        let mem = vec![0; width * height * 4];
+        let total = 0;
+
+        Mandelbrot {
+            fractal,
+            hist,
+            mem,
+            total,
+            width,
+            height,
+        }
     }
 
-    pub fn get_iterations(x: f32, y: f32) -> usize {
+    pub fn get_hist(&self) -> Vec<u32> {
+        self.hist.clone()
+    }
+
+    fn get_iterations(x: f32, y: f32) -> usize {
         let mut z = Complex::new(0.0, 0.0);
         let c = Complex::new(x, y);
 
@@ -86,14 +72,14 @@ impl Mandelbrot {
         let trans_x = span_x / 2.0 - x2;
         let trans_y = -(span_y / 2.0 + y2);
 
-        for j in 0..HEIGHT {
-            for i in 0..WIDTH {
-                let scale_x = (span_x * i as f32 / WIDTH as f32 - span_x / 2.0) - trans_x;
-                let scale_y = -(span_y * j as f32 / HEIGHT as f32 - span_y / 2.0) - trans_y;
+        for j in 0..self.height {
+            for i in 0..self.width {
+                let scale_x = (span_x * i as f32 / self.width as f32 - span_x / 2.0) - trans_x;
+                let scale_y = -(span_y * j as f32 / self.height as f32 - span_y / 2.0) - trans_y;
 
                 let iterations = Mandelbrot::get_iterations(scale_x, scale_y);
 
-                self.fractal[j * WIDTH + 1] = iterations as u32;
+                self.fractal[j * self.width + i] = iterations as u32;
 
                 if iterations != MAX_ITERATIONS {
                     self.hist[iterations] += 1;
@@ -104,5 +90,36 @@ impl Mandelbrot {
         for i in 0..MAX_ITERATIONS {
             self.total += self.hist[i];
         }
+    }
+
+    pub fn write_to_bitmap(&mut self) {
+        let mut row = self.mem.iter_mut();
+        let mut pixel: Vec<&mut u8> = row.by_ref().take(4).collect();
+
+        for j in 0..self.height {
+            for i in 0..self.width {
+                let iterations = self.fractal[j * self.width + i] as usize;
+                let mut blue = 0;
+                let mut green = 0;
+
+                if iterations != MAX_ITERATIONS {
+                    let mut hue = 0.0;
+                    for i in 0..iterations {
+                        hue += (self.hist[i] as f32) / self.total as f32;
+                    }
+                    blue = f32::powf(255.0, hue) as u8;
+                    green = f32::powf(186.0, hue) as u8;
+                }
+
+                *pixel[1] = green;
+                *pixel[2] = blue;
+
+                pixel = row.by_ref().take(4).collect();
+            }
+        }
+    }
+
+    pub fn bitmap(&mut self) -> *const u8 {
+        self.mem.as_ptr()
     }
 }
--- tests/web.rs
+++ tests/web.rs
@@ -7,7 +7,22 @@ use wasm_bindgen_test::*;
 
 wasm_bindgen_test_configure!(run_in_browser);
 
+// #[wasm_bindgen_test]
+// fn pass() {
+//     assert_eq!(1 + 1, 2);
+// }
+
+extern crate wasm_mandelbrot;
+use wasm_mandelbrot::CoordinatePair;
+use wasm_mandelbrot::Mandelbrot;
+
 #[wasm_bindgen_test]
-fn pass() {
-    assert_eq!(1 + 1, 2);
+pub fn test_get_iterations() {
+    // Origin should reach max iterations
+    let out1 = Mandelbrot::get_iterations(0.0, 0.0);
+    assert_eq!(1000, out1);
+
+    // 3,3 should coverge to 0
+    let out2 = Mandelbrot::get_iterations(3.0, 3.0);
+    assert_eq!(0, out2);
 }
