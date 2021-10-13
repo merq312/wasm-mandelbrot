90bd307d0723fa7004cff8a2a4feddb937cba6b6
Chamila Gunasena <merc312@gmail.com>
2021-10-12 20:27:13 -0400

    feat: select pair of coordinates to zoom into fractal

--- www/index.ts
+++ www/index.ts
@@ -1,80 +1,172 @@
-import { Mandelbrot } from 'wasm-mandelbrot'
-import { memory } from 'wasm-mandelbrot/wasm_mandelbrot_bg.wasm'
+import {Mandelbrot} from 'wasm-mandelbrot'
+import {memory} from 'wasm-mandelbrot/wasm_mandelbrot_bg.wasm'
 
 const WIDTH = 800
 const HEIGHT = 600
 
-class CoordinatePair {
-  x1: number
-  x2: number
-  y1: number
-  y2: number
+class Pair {
+    x: number
+    y: number
 
-  constructor(x1: number, y1: number, x2: number, y2: number) {
-    this.x1 = x1
-    this.y1 = y1
-    this.x2 = x2
-    this.y2 = y2
-  }
+    constructor(x: number, y: number) {
+        this.x = x
+        this.y = y
+    }
 
-  transform() {
-    this.x1 = (2.0 * this.x1) / HEIGHT - 1.6
-    this.x2 = (2.0 * this.x2) / HEIGHT - 1.6
-    this.y1 = (2.0 * this.y1) / HEIGHT - 0.4
-    this.y2 = (2.0 * this.y2) / HEIGHT - 0.4
-  }
+    transform() {
+        // Both coords are scaled by HEIGHT.
+        // They need to be scaled by the same factor preserve proportions
+        this.x = (2.0 * this.x) / HEIGHT - 2
+        this.y = (2.0 * this.y) / HEIGHT - 1
+    }
 }
 
-// ******************************************
+// Swap coordinates if they are backwards
+const swapPair = (p1: Pair, p2: Pair) => {
+    if (p2.x < p1.x) {
+        const temp = p1.x;
+        p1.x = p2.x;
+        p2.x = temp;
 
-const startGenerate = performance.now()
-const m = Mandelbrot.new(WIDTH, HEIGHT)
-const c = new CoordinatePair(0, 0, WIDTH, HEIGHT)
-c.transform()
+        // p1.x = p1.x + p2.x;
+        // p2.x = p1.x - p2.x;
+        // p1.x = p1.x - p2.x;
+    }
+    if (p2.y < p1.y) {
+        const temp = p1.y;
+        p1.y = p2.y;
+        p2.y = temp;
 
-m.draw_fractal(c.x1, c.y1, c.x2, c.y2)
-m.write_to_bitmap()
-const endGenerate = performance.now()
+        // p1.y = p1.y + p2.y;
+        // p2.y = p1.y - p2.y;
+        // p1.y = p1.y - p2.y;
+    }
+}
 
-// ******************************************
+const fixRatio = (p1: Pair, p2: Pair) => {
+    const targetRatio = 4/3
 
-const startDraw = performance.now()
+    const diffX = (p2.x-p1.x)
+    const diffY  = (p2.y-p1.y)
+
+    if (diffX >= diffY) {
+        p2.x = p1.x + (diffY * targetRatio)
+    } else {
+        p2.y = p1.y + (diffX / targetRatio)
+    }
+}
 
 const canvas: HTMLCanvasElement = <any>document.getElementById('canvas')!
+const zoomBtn: HTMLButtonElement = <any>document.getElementById('zoomBtn')!
 const ctx: CanvasRenderingContext2D = canvas.getContext('2d')!
 const imageData = ctx.createImageData(WIDTH, HEIGHT)
 
+const m = Mandelbrot.new(WIDTH, HEIGHT)
 const bitmapPtr = m.bitmap()
 const bitmap = new Uint8Array(memory.buffer, bitmapPtr, WIDTH * HEIGHT * 4)
 
-// Iterate through every pixel
-for (let i = 0; i < imageData.data.length; i += 4) {
-  imageData.data[i + 0] = 0
-  imageData.data[i + 1] = bitmap[i + 1]
-  imageData.data[i + 2] = bitmap[i + 2]
-  imageData.data[i + 3] = 255
+let clickCounter = 0
+let pairFirst: Pair = new Pair(0, 0)
+let pairSecond: Pair = new Pair(0, 0)
+
+const generateFractal = (m: Mandelbrot, p1: Pair, p2: Pair) => {
+    const startGenerate = performance.now()
+
+    swapPair(p1, p2)
+    fixRatio(p1, p2)
+    p1.transform()
+    p2.transform()
+
+    console.log(`x1: ${p1.x.toFixed(2)}, y1: ${p1.y.toFixed(2)}`)
+    console.log(`x2: ${p2.x.toFixed(2)}, y2: ${p2.y.toFixed(2)}`)
+
+    m.draw_fractal(p1.x, p1.y, p2.x, p2.y)
+    m.write_to_bitmap()
+
+    const endGenerate = performance.now()
+    const generate: HTMLParagraphElement = <any>document.getElementById('generate')!
+    generate.textContent = `Generation took ${(endGenerate - startGenerate).toFixed(
+        2
+    )} milliseconds`
 }
 
+const drawFractal = () => {
+    const startDraw = performance.now()
+
+// Iterate through every pixel
+    for (let i = 0; i < imageData.data.length; i += 4) {
+        imageData.data[i] = 0
+        imageData.data[i + 1] = bitmap[i + 1] ?? 0
+        imageData.data[i + 2] = bitmap[i + 2] ?? 0
+        imageData.data[i + 3] = 255
+    }
+
 // Draw image data to the canvas
-ctx.putImageData(imageData, 0, 0)
-const endDraw = performance.now()
+    ctx.putImageData(imageData, 0, 0)
+    const endDraw = performance.now()
 
-// ******************************************
+    const draw: HTMLParagraphElement = <any>document.getElementById('draw')!
 
-const generate: HTMLParagraphElement = <any>document.getElementById('generate')!
-const draw: HTMLParagraphElement = <any>document.getElementById('draw')!
+    draw.textContent = `Drawing took ${(endDraw - startDraw).toFixed(
+        2
+    )} milliseconds`
+}
 
-generate.textContent = `Generation took ${(endGenerate - startGenerate).toFixed(
-  2
-)} milliseconds`
-draw.textContent = `Drawing took ${(endDraw - startDraw).toFixed(
-  2
-)} milliseconds`
+const drawVertLine = (x: number) => {
+    x = Math.floor(x)
+    for (let i = 0; i < imageData.data.length; i += 4) {
+        if (i % (4*WIDTH) == 4*x) {
+            imageData.data[i] = 255
+            imageData.data[i + 1] = 255
+            imageData.data[i + 2] = 255
+            imageData.data[i + 3] = 255
+        }
+    }
+    ctx.putImageData(imageData, 0, 0)
+}
 
-// Testing hist
-// const hist = m.get_hist();
-// let str: String = "";
+const drawHorsLine = (y: number) => {
+    y = Math.floor(y)
+    for (let i = 0; i < imageData.data.length; i += 4) {
+        if (Math.floor(i / (4 * 800)) == y) {
+            imageData.data[i] = 255
+            imageData.data[i + 1] = 255
+            imageData.data[i + 2] = 255
+            imageData.data[i + 3] = 255
+        }
+    }
+    ctx.putImageData(imageData, 0, 0)
+}
 
-// hist.forEach((ea) => (str += ea.toString() + ", "));
+// Records and draws coordinate pair for zoom in
+canvas.addEventListener('click', (event) => {
+    const boundingRect = canvas.getBoundingClientRect()
 
-// console.log(str);
+    const canvasLeft = (event.clientX - boundingRect.left)
+    const canvasTop = (event.clientY - boundingRect.top)
+
+    if (clickCounter == 0) {
+        pairFirst.x = canvasLeft
+        pairFirst.y = canvasTop
+        drawVertLine(canvasLeft)
+        drawHorsLine(canvasTop)
+        clickCounter = 1
+    } else if (clickCounter == 1) {
+        pairSecond.x = canvasLeft
+        pairSecond.y = canvasTop
+
+        drawVertLine(canvasLeft)
+        drawHorsLine(canvasTop)
+
+        clickCounter = 2
+    }
+})
+
+zoomBtn.addEventListener('click', () => {
+    generateFractal(m, pairFirst, pairSecond)
+    drawFractal()
+    clickCounter = 0
+})
+
+generateFractal(m, new Pair(0, 0), new Pair(WIDTH, HEIGHT))
+drawFractal()
